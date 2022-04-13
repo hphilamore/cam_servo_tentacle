bd79a0ad7a497d555a62fae88f9c950b7971efd2
hphilamore <hemmaphilamore@gmail.com>
2022-04-13 16:36:50 +0100

    initial commit, env files removed

---
+++ test.py
@@ -0,0 +1,15 @@
+from huskylib import HuskyLensLibrary            # import class
+hl = HuskyLensLibrary("I2C", "", address=0x32)   # set communication channel 
+#hl.algorthim("ALGORITHM_FACE_RECOGNITION")
+
+print(hl.knock())
+print(hl.requestAll())
+
+while(1):
+    data=hl.requestAll()
+    x = 0
+    for i in data:
+        x = x+1
+        # x range (0, 320)
+        # y range (0, 240)
+        print(i.ID, i.x, i.y, i.width, i.height)
